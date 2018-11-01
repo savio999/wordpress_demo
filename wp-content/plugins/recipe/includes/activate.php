b07a55fa352d0d2c84b79f9ea4bce2475775f840
Savio Menezes <savio.menezes@kodework.com>
2018-11-01 17:31:08 +0530

    changes done along with db backup

--- wp-content/plugins/recipe/includes/activate.php
+++ wp-content/plugins/recipe/includes/activate.php
@@ -3,5 +3,11 @@ function r_register_hook(){
     if(version_compare(get_bloginfo('version'), '4.5', '<')){
         wp_die(__('You must update wordpress to use this plugin.', 'recipe' ));
     }
+    
+    global $wpdb;
+    
+    $sql_query = "CREATE TABLE `".$wpdb->prefix."recipe_ratings` ( `ID` BIGINT(20) NULL AUTO_INCREMENT , `recipe_id` BIGINT(20) NOT NULL , `rating` FLOAT(3,2) NOT NULL , `user_ip` VARCHAR(32) NOT NULL , PRIMARY KEY (`ID`)";
+
+    require_once ABSPATH . '/wp-admin/';
 }
 
--- wp-content/plugins/recipe/process/filter-content.php
+++ wp-content/plugins/recipe/process/filter-content.php
@@ -1,3 +1,22 @@
 <?php
-function r_filter_recipe_content(){
+function r_filter_recipe_content($content){
+    if( ! (is_singular('recipe'))){
+        return $content;
+    }
+    
+    global $post;
+    $recipe_template = file_get_contents('recipe-template.php',true);
+    $recipe_data = get_post_meta($post->ID,'recipe_data',true);
+    $recipe_template = str_replace('INGREDIENTS_PH', $recipe_data['ingredients'], $recipe_template);
+    $recipe_template = str_replace('COOKING_TIME_PH', $recipe_data['time'], $recipe_template);
+    $recipe_template = str_replace('UTENSILS_PH', $recipe_data['utensils'], $recipe_template);
+    $recipe_template = str_replace('LEVEL_PH', $recipe_data['level'], $recipe_template);
+    $recipe_template = str_replace('TYPE_PH', $recipe_data['meal_type'], $recipe_template);
+    $recipe_template = str_replace('INGREDIENTS_I18N', 'Ingredients', $recipe_template);
+    $recipe_template = str_replace('COOKING_TIME_I18N', 'Cooking time', $recipe_template);
+    $recipe_template = str_replace('UTENSILS_I18N', 'Utensils', $recipe_template);
+    $recipe_template = str_replace('LEVEL_I18N','Level', $recipe_template);
+    $recipe_template = str_replace('TYPE_I18N', 'Meal Type', $recipe_template);    
+    $recipe_template = str_replace('RATE_I18N', 'Rating', $recipe_template);      
+    return  $recipe_template . $content;
 }
